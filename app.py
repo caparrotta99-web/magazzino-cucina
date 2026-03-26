7f853a6cc0ab8a1b75e271694d5ffd0856e63e92
simone caparrotta <simonecaparrotta@Mac.home>
2026-03-27 00:49:08 +0100

    Fix porta Render

--- app.py
+++ app.py
@@ -624,4 +624,5 @@ def api_scan_label():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(debug=True, host='0.0.0.0', port=port)
